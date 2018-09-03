d41bba85d34f20b275c82e549b643cd992a06e1e
JohnLockhart <lockhartjohn527@gmail.com>
2018-09-03 10:13:52 -0700

    Add more examples

--- bigO.rb
+++ bigO.rb
@@ -23,10 +23,57 @@ def add(a, b)
   a - b
 end
 
-#2
+#2 
 def print_arr_1(arr)
   arr.each do |idx|
     puts idx
   end
+end
+
+#3 
+def print_arr_2(arr)
+  arr.each_with_index do |el, idx|
+    break if idx == arr.length/2 - 1
+    puts el
+  end
+end
+
+#4
+def print_arr_3(arr)
+  arr.each do |el|
+    break if el == arr.length/2 - 1
+    puts el
+  end
+end
+
+#5
+def print_arr_4(arr)
+  arr.each do |el|
+    break if el == arr.length/2 - 1
+    puts el
+  end
+
+  arr.each_with_index do |el, idx|
+    puts el if idx % 3 == 0
+  end
+
+  puts arr.last
+end
+
+#6
+def search(arr, target)
+  arr.each_with_index do |el, idx|
+    return idx if el == target
+  end
+end
+
+#7
+def searchity_search(arr, target)
+  results = []
+  arr.each do |el|
+    results << search(arr, target + el)
+  end
+
+  results  
 end
 
--- notes.md
+++ notes.md
@@ -1,2 +1,8 @@
 1. given two numbers, do one thing or the other one time. time complexity O(1); same for space
 2. iterating over the array, so in the worst case, time complexity depends on the number of elements in the array, n. time complexity is 'linear', aka O(n).
+3. Iterating through an array, will divide by 2, n/2 but n dominates so it's just O (n)
+4. Best case: constant. If the element we're looking for is the first one in the array, it'll meet the break condition and exit immediately.
+Worst case: linear. The loop will iterate through every element in the array if the element we're looking for is the last one or not present in the array at all.
+5. have to do two iteractions with all the .each, but that's 2n, which reduces to O(n)
+6. best case is 1, worst case is n
+7.
